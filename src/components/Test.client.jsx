1f99bd5b6d69303679262abfb72e35baf4f1e09e
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-15 17:36:35 +0100

    use client

--- src/components/Test.client.jsx
+++ src/components/Test.client.jsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { useEffect } from "react";
+
 function Test({ children }) {
+  useEffect(() => {
+    console.log("use effect");
+  }, []);
   return <div className="dom-test">{children}</div>;
 }
 export default Test;
