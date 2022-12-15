69b936bba4c86abaf22a39ca6e89d2164ef19f84
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-15 11:25:55 +0100

    .

--- src/components/Test.client.jsx
+++ src/components/Test.client.jsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 function Test({ children }) {
+  useEffect(() => {
+    console.log("TEST RENDERED");
+  }, []);
+
   return <div className="dom-test">{children}</div>;
 }
 export default Test;
