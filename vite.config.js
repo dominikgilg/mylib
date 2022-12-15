75c646e1fe5c5867af52ff2b13d846af43f258ee
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-15 10:41:34 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.js"),
       name: "@nosto/lib-test",
-      fileName: "index",
+      fileName: "lib-test",
     },
     rollupOptions: {
       external: ["react", "react-dom"],
