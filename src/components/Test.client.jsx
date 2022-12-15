756700c7ef1125886040c1eb91e6ec359d99c20a
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-15 15:52:52 +0100

    .

--- src/components/Test.client.jsx
+++ src/components/Test.client.jsx
@@ -1,9 +1,16 @@
 import { useEffect } from "react";
+import { useClientOnly } from "react-client-only";
 
 function Test({ children }) {
+  const isOnClient = useClientOnly();
+
   useEffect(() => {
     console.log("RENDER TEST");
   }, []);
+
+  if (!isOnClient) {
+    return null;
+  }
 
   return <div className="dom-test">{children}</div>;
 }
