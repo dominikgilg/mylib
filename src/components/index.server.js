4ff633592fac6352857fbf88231d866b1550ab18
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-19 11:44:50 +0100

    .

--- src/components/index.server.js
+++ src/components/index.server.js
@@ -1,3 +1,4 @@
 //EXPORT SERVER COMPONENTS
 
 export { default as NostoSession } from "./server/NostoSession.server";
+export { default as NostoServerTest } from "./server/Test.server";
---
+++ src/components/server/Test.server.jsx
@@ -0,0 +1,34 @@
+// import { NostoSession as NostoComponent } from "@nosto/nosto-react";
+// import { useShopQuery, gql, useSession, useShop } from "@shopify/hydrogen";
+// import { createHash } from "crypto";
+
+export default function NostoServerTest(props) {
+  console.log("NOSTO SESSION!!!");
+
+  //   const { storeDomain } = useShop();
+  //   const { customerAccessToken } = useSession();
+  //   const QUERY = gql`
+  //           query {
+  //           customer(customerAccessToken: "${customerAccessToken}") {
+  //             firstName
+  //             lastName
+  //             email
+  //             acceptsMarketing
+  //             id
+  //           }
+  //         }
+  //         `;
+  //   const {
+  //     data: { customer: customerData },
+  //   } = useShopQuery({
+  //     query: QUERY,
+  //   });
+
+  // if (customerData?.id && storeDomain) {
+  //   customerData.customerReference = createHash("sha256")
+  //     .update(customerData.id + storeDomain)
+  //     .digest("hex");
+  // }
+  return <div>NOSTO SERVER TEST</div>;
+  //   return <NostoComponent customerData={customerData} type="NostoSession" />;
+}
