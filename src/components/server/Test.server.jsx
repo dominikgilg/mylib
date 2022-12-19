f2be3619af31e2eeda7d08d97fdedd047dda659e
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-19 16:44:34 +0100

    .

--- src/components/server/Test.server.jsx
+++ src/components/server/Test.server.jsx
@@ -1,6 +1,6 @@
-// import { NostoSession as NostoComponent } from "@nosto/nosto-react";
+import { NostoSession as NostoComponent } from "@nosto/nosto-react";
 import { useShopQuery, gql, useSession, useShop } from "@shopify/hydrogen";
-import Crypto from "node:crypto";
+// import Crypto from "node:crypto";
 
 export default function NostoServerTest(props) {
   console.log("NOSTO SESSION!!! :)");
@@ -26,14 +26,14 @@ export default function NostoServerTest(props) {
 
   console.log(customerData?.id);
 
-  if (customerData?.id && storeDomain) {
-    customerData.customerReference = Crypto.createHash("sha256")
-      .update(customerData.id + storeDomain)
-      .digest("hex");
-  }
+  //   if (customerData?.id && storeDomain) {
+  //     customerData.customerReference = Crypto.createHash("sha256")
+  //       .update(customerData.id + storeDomain)
+  //       .digest("hex");
+  //   }
 
-  console.log("REFEERNCE: ", customerData?.customerReference);
+  //   console.log("REFEERNCE: ", customerData?.customerReference);
 
-  return <div>NOSTO SERVER TEST</div>;
-  //   return <NostoComponent customerData={customerData} type="NostoSession" />;
+  //   return <div>NOSTO SERVER TEST</div>;
+  return <NostoComponent customerData={customerData} type="NostoSession" />;
 }
