61107a97f78493d5da6f7e4f13c94f88fdef31c2
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-20 14:09:04 +0100

    .

--- src/components/server/Test.server.jsx
+++ src/components/server/Test.server.jsx
@@ -2,7 +2,7 @@
 import { useShopQuery, gql, useSession, useShop } from "@shopify/hydrogen";
 // import Crypto from "node:crypto";
 // import { TestComponent } from "@nosto/test-component";
-import { NostoComponent } from "@nosto/nosto-react";
+import { NostoSession as NostoComponent } from "@nosto/nosto-react";
 
 export default function (props) {
   console.log("NOSTO SESSION!!! :)");
