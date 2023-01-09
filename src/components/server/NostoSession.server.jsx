5ae3235b5ddc2f3526c285a2bdeccba85def328b
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-01-09 16:05:37 +0100

    .

--- src/components/server/NostoSession.server.jsx
+++ src/components/server/NostoSession.server.jsx
@@ -1,7 +1,6 @@
 import { NostoSession as NostoComponent } from "@nosto/nosto-react/hydrogen";
 import { useShopQuery, gql, useSession, useShop } from "@shopify/hydrogen";
-import { sha256 } from "sha256";
-// import Crypto from "crypto";
+import { sha256 } from "js-sha256";
 
 export default function NostoSession(props) {
   console.log("NOSTO SESSION!!!");
@@ -30,12 +29,6 @@ export default function NostoSession(props) {
   if (customerData?.id && storeDomain) {
     customerData.customerReference = sha256(customerData.id + storeDomain);
   }
-
-  // if (customerData?.id && storeDomain) {
-  //   customerData.customerReference = Crypto.createHash("sha256")
-  //     .update(customerData.id + storeDomain)
-  //     .digest("hex");
-  // }
 
   return <NostoComponent customerData={customerData} type="NostoSession" />;
 }
