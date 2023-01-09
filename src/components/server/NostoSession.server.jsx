e336aa2ec4c49d850f3ec6d728d62ec740624b57
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-01-09 14:25:30 +0100

    .

--- src/components/server/NostoSession.server.jsx
+++ src/components/server/NostoSession.server.jsx
@@ -1,4 +1,4 @@
-import { NostoSession as NostoComponent } from "@nosto/nosto-react";
+import { NostoSession as NostoComponent } from "@nosto/nosto-react/hydrogen";
 import { useShopQuery, gql, useSession, useShop } from "@shopify/hydrogen";
 import { sha256 } from "js-sha256";
 
