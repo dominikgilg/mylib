b145c6f8e8aa7fc6ea1e673f23a4a1df27359c87
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-01-09 21:11:31 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-export * from "../dist/es/index.client";
+// export * from "../dist/es/index.client";
 // export * from "../dist/es/index.server";
-// export * from "./components/index.client.js";
+export * from "./components/index.client.js";
 export * from "./components/index.server.js";
+//
 // export { default as nostoHydrogen } from "./plugin";
