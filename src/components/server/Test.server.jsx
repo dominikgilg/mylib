cf10ed186ba5850948f072808f79cad0ece10b0a
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-01-09 13:22:56 +0100

    .

--- src/components/server/Test.server.jsx
+++ src/components/server/Test.server.jsx
@@ -2,8 +2,8 @@
 import { useShopQuery, gql, useSession, useShop } from "@shopify/hydrogen";
 // import Crypto from "node:crypto";
 import { sha256 } from "js-sha256";
-import { TestComponent as NostoComponent } from "@nosto/test-component";
-// import { NostoSession as NostoComponent } from "@nosto/nosto-react/prebuild";
+// import { TestComponent as NostoComponent } from "@nosto/test-component";
+import { NostoSession as NostoComponent } from "@nosto/nosto-react/prebuild";
 
 export default function (props) {
   console.log("NOSTO SESSION!!! :)");
