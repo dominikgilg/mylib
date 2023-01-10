58d15b880bec74557fc9dc1c08bcc870509d4df4
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-01-10 16:10:30 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // export * from "./components/index.client.js";
-export * from "./components/index.server.js";
+// export * from "./components/index.server.js";
 
 export * from "../dist/es/index.client.js";
-// export * from "../dist/es/index.server.js";
+export * from "../dist/es/index.server.js";
